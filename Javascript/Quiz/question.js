04d6536a784ef8ab14b69f60fdc29f84ca6929b1
akosuab86 <akosuab86@gmail.com>
2019-12-27 18:06:15 -0500

    Javascript projects

---
+++ Javascript/Quiz/question.js
@@ -0,0 +1,16 @@
+var questions = [{
+	"question": "My name is",
+	"option1": "Akosua",
+	"option2": "Titus",
+	"option3": "Oswald",
+	"option4": "Bella",
+	"answer": "1"
+}, {
+	"question": "My age is",
+	"option1": "20",
+	"option2": "21",
+	"option3": "22",
+	"option4": "23",
+	"answer": "4"
+
+}]
